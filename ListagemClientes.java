74ffdc8899a2fe2a6b09903cf67a4ddff9fe4288
Renato Rodrigues <renato.rojunior@gmail.com>
2024-01-23 20:24:40 -0300

    Listagem de Clientes com Caracterísca Etária.

--- ListagemClientes.java
+++ ListagemClientes.java
@@ -1,3 +1,5 @@
+import java.time.Year;
+
 public class ListagemClientes {
     public static void main(String[] args) {
         // Cliente 1
@@ -6,15 +8,46 @@ public class ListagemClientes {
 
         // Cliente 2
         String nomeCliente2 = "Sicrano";
-        int anoNascimentoCliente2 = 1990;
+        int anoNascimentoCliente2 = 1995;
+
+        // Calcular idade
+        int idadeCliente1 = calcularIdade(anoNascimentoCliente1);
+        int idadeCliente2 = calcularIdade(anoNascimentoCliente2);
+
+        // Determinar característica etária
+        String caracteristicaCliente1 = getCaracteristicaEtaria(idadeCliente1);
+        String caracteristicaCliente2 = getCaracteristicaEtaria(idadeCliente2);
 
         // Listagem dos clientes
         System.out.println(">>> Listagem dos Clientes");
         System.out.println("--------------------------------------------------");
         System.out.println("Nome: " + nomeCliente1);
         System.out.println("Data de Nascimento: " + anoNascimentoCliente1);
+        System.out.println("Idade: " + idadeCliente1);
+        System.out.println("Característica Etária: " + caracteristicaCliente1);
         System.out.println("--------------------------------------------------");
         System.out.println("Nome: " + nomeCliente2);
         System.out.println("Data de Nascimento: " + anoNascimentoCliente2);
+        System.out.println("Idade: " + idadeCliente2);
+        System.out.println("Característica Etária: " + caracteristicaCliente2);
+    }
+
+    // Método para calcular a idade do cliente
+    private static int calcularIdade(int anoNascimento) {
+        int anoAtual = Year.now().getValue();
+        return anoAtual - anoNascimento;
+    }
+
+    // Método para determinar a característica etária do cliente
+    private static String getCaracteristicaEtaria(int idade) {
+        if (idade <= 17) {
+            return "ADOLESCENTE";
+        } else if (idade >= 18 && idade <= 29) {
+            return "JOVEM";
+        } else if (idade >= 30 && idade <= 59) {
+            return "ADULTO";
+        } else {
+            return "IDOSO";
+        }
     }
 }
