772e53eff88747842ffaefcd83be3103e0c94acd
Renato Rodrigues <renato.rojunior@gmail.com>
2024-01-23 20:08:27 -0300

    Criando uma listagem de clientes.

---
+++ ListagemClientes.java
@@ -0,0 +1,20 @@
+public class ListagemClientes {
+    public static void main(String[] args) {
+        // Cliente 1
+        String nomeCliente1 = "Fulano";
+        int anoNascimentoCliente1 = 1980;
+
+        // Cliente 2
+        String nomeCliente2 = "Sicrano";
+        int anoNascimentoCliente2 = 1990;
+
+        // Listagem dos clientes
+        System.out.println(">>> Listagem dos Clientes");
+        System.out.println("--------------------------------------------------");
+        System.out.println("Nome: " + nomeCliente1);
+        System.out.println("Data de Nascimento: " + anoNascimentoCliente1);
+        System.out.println("--------------------------------------------------");
+        System.out.println("Nome: " + nomeCliente2);
+        System.out.println("Data de Nascimento: " + anoNascimentoCliente2);
+    }
+}
