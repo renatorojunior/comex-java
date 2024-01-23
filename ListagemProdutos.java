dd25e109c0611023d5b3d0af7ccd7488853c521b
Renato Rodrigues <renato.rojunior@gmail.com>
2024-01-23 20:14:32 -0300

    Criando uma listagem de produtos.

---
+++ ListagemProdutos.java
@@ -0,0 +1,20 @@
+public class ListagemProdutos {
+      public static void main(String[] args) {
+        // Produto 1
+        String nomeProduto1 = "Livro";
+        double precoProduto1 = 49.95;
+
+        // Produto 2
+        String nomeProduto2 = "Celular";
+        double precoProduto2 = 1470.76;
+
+        // Listagem dos produtos
+        System.out.println(">>> Listagem dos Produtos");
+        System.out.println("--------------------------------------------------");
+        System.out.println("Nome: " + nomeProduto1);
+        System.out.println("Preço do Produto: R$ " + precoProduto1);
+        System.out.println("--------------------------------------------------");
+        System.out.println("Nome: " + nomeProduto2);
+        System.out.println("Preço do Produto: R$ " + precoProduto2);
+    }
+}
